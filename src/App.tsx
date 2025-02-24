ec22a72e7e8f74517a4f96ab54d2c7dabd72cb30
Sandy Arib Ahmad <ahmadsandyarib@gmail.com>
2025-02-24 18:51:12 +0700

    add h1 heading

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <ul>
+      <h1>Supabase + React</h1>
       {instruments.map((instrument) => (
         <li key={instrument.name}>{instrument.name}</li>
       ))}
