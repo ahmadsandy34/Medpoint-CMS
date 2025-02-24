bf33b8805b7df6e32ee3e7be4a065ed9cba0d87a
Sandy Arib Ahmad <ahmadsandyarib@gmail.com>
2025-02-24 18:00:15 +0700

    fiturab

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <ul>
-      <h2>Instruments</h2>
+      <h2>List of Instruments</h2>
       {instruments.map((instrument) => (
         <li key={instrument.name}>{instrument.name}</li>
       ))}
